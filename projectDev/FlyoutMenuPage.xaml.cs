11350710ccb279f52063b8bc2989072af9393407
Ram-Delos-Santos <136768232+Ram-Delos-Santos@users.noreply.github.com>
2024-06-12 11:00:39 +0800

    Add files via upload

---
+++ projectDev/FlyoutMenuPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace projectDev
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FlyoutMenuPage : ContentPage
+    {
+        ObservableCollection<FlyoutItemPage> items;
+
+        public FlyoutMenuPage()
+        {
+            InitializeComponent();
+
+            items = new ObservableCollection<FlyoutItemPage>
+            {
+                new FlyoutItemPage {IconSource = "home.png", Title="Home", TargetPage=typeof(HomePage)},
+                new FlyoutItemPage {IconSource = "item.png", Title="Item List", TargetPage=typeof(ItemPage)},
+                new FlyoutItemPage {IconSource = "setting.png", Title="Account Settings", TargetPage = typeof(SettingPage)},
+                new FlyoutItemPage {IconSource = "about.png", Title="About", TargetPage = typeof(AboutPage)}
+            };
+            myListView.ItemsSource = items;
+        }
+
+        async void MyListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item is FlyoutItemPage selectedItem)
+            {
+                Page page = (Page)Activator.CreateInstance(selectedItem.TargetPage);
+                await Navigation.PushAsync(page);
+            }
+
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
---
+++ projectDev/Model/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Xamarin.Forms;
+using System.IO;
+namespace projectDev.Model
+{
+    public class ProductModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
